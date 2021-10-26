95bf5aee9b892d22c87a24d3a25a0681479a8c38
Valentin C <valentin.craciun99@gmail.com>
2021-10-26 15:44:43 +0300

    add test for basic types

--- serde/deserializar_test.go
+++ serde/deserializar_test.go
@@ -4,12 +4,14 @@ import (
 	"github.com/ElrondNetwork/elrond-sdk-erdgo/serde/testingMocks"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
+	"math/big"
 	"testing"
 )
 
 const (
 	SRC_BASIC_TYPES       = "./testingMocks/basicDataTypes"
 	SRC_NESTED_STRUCTURES = "./testingMocks/nestedStructures"
+	SRC_PRIMITIVE         = "./testingMocks/basicTypeBigInt"
 )
 
 func TestDeserializer_CreateStruct_BasicTypes(t *testing.T) {
@@ -40,6 +42,22 @@ func TestDeserializer_CreateStruct_NestedStructures(t *testing.T) {
 	bt := &testingMocks.NestedStructure{}
 
 	_, err = ds.CreateStruct(bt, data)
+
+	assert.Nil(t, err)
+}
+
+func Test_deserializer_CreatePrimitiveDataType(t *testing.T) {
+	t.Parallel()
+
+	data, err := ioutil.ReadFile(SRC_PRIMITIVE)
+	assert.Nil(t, err)
+	assert.NotNil(t, data)
+
+	ds := NewDeserializer()
+
+	bigInt := &big.Int{}
+
+	err = ds.CreatePrimitiveDataType(bigInt, data)
 
 	assert.Nil(t, err)
 }
