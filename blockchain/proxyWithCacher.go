9885e7b88d2824d5ed45a666bde59a01d840156e
Rebegea Dragos-Alexandru <dragosalex97@gmail.com>
2021-12-22 15:22:41 +0200

    proxy with cacher

---
+++ blockchain/proxyWithCacher.go
@@ -0,0 +1,96 @@
+package blockchain
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/ElrondNetwork/elrond-go-core/core/check"
+	logger "github.com/ElrondNetwork/elrond-go-logger"
+	"github.com/ElrondNetwork/elrond-sdk-erdgo/data"
+)
+
+var log = logger.GetOrCreate("elrond-sdk-erdgo/proxyWithCacher")
+
+const minimumCachingInterval = time.Second
+
+type ArgsElrondProxyWithCache struct {
+	Proxy
+	CacheExpiryDuration time.Duration
+}
+
+type elrondProxyWithCache struct {
+	Proxy
+	mut                 sync.RWMutex
+	fetchedConfigs      *data.NetworkConfig
+	lastFetchedTime     time.Time
+	cacheExpiryDuration time.Duration
+	sinceTimeHandler    func(t time.Time) time.Duration
+}
+
+// NewElrondProxyWithCache will create an elrond proxy with cache instance
+func NewElrondProxyWithCache(proxy Proxy, expirationTime time.Duration) (*elrondProxyWithCache, error) {
+	if check.IfNil(proxy) {
+		return nil, ErrNilProxy
+	}
+	if expirationTime < minimumCachingInterval {
+		return nil, ErrInvalidCacherDuration
+	}
+	return &elrondProxyWithCache{
+		Proxy:               proxy,
+		cacheExpiryDuration: expirationTime,
+		sinceTimeHandler:    since,
+	}, nil
+}
+
+func since(t time.Time) time.Duration {
+	return time.Since(t)
+}
+
+// GetNetworkConfig will return the cached network configs fetching new values and saving them if necessary
+func (proxy *elrondProxyWithCache) GetNetworkConfig(ctx context.Context) (*data.NetworkConfig, error) {
+	proxy.mut.RLock()
+	cachedConfigs := proxy.getCachedConfigs()
+	proxy.mut.RUnlock()
+
+	if cachedConfigs != nil {
+		return cachedConfigs, nil
+	}
+
+	return proxy.cacheConfigs(ctx)
+}
+
+func (proxy *elrondProxyWithCache) getCachedConfigs() *data.NetworkConfig {
+	if proxy.sinceTimeHandler(proxy.lastFetchedTime) > proxy.cacheExpiryDuration {
+		return nil
+	}
+
+	return proxy.fetchedConfigs
+}
+
+func (proxy *elrondProxyWithCache) cacheConfigs(ctx context.Context) (*data.NetworkConfig, error) {
+	proxy.mut.Lock()
+	defer proxy.mut.Unlock()
+
+	// maybe another parallel running go routine already did the fetching
+	cachedConfig := proxy.getCachedConfigs()
+	if cachedConfig != nil {
+		return cachedConfig, nil
+	}
+
+	log.Debug("Network config not cached. caching...")
+	configs, err := proxy.Proxy.GetNetworkConfig(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	proxy.lastFetchedTime = time.Now()
+	proxy.fetchedConfigs = configs
+
+	return configs, nil
+}
+
+// IsInterfaceNil returns true if there is no value under the interface
+func (proxy *elrondProxyWithCache) IsInterfaceNil() bool {
+	return proxy == nil
+}
---
+++ blockchain/proxyWithCacher_test.go
@@ -0,0 +1,157 @@
+package blockchain
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/ElrondNetwork/elrond-sdk-erdgo/data"
+	"github.com/ElrondNetwork/elrond-sdk-erdgo/interactors/mock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewElrondProxyWithCache(t *testing.T) {
+	t.Parallel()
+
+	t.Run("nil proxy", func(t *testing.T) {
+		t.Parallel()
+
+		nth, err := NewElrondProxyWithCache(nil, time.Minute)
+		assert.True(t, nth.IsInterfaceNil())
+		assert.Equal(t, ErrNilProxy, err)
+	})
+	t.Run("invalid caching duration", func(t *testing.T) {
+		t.Parallel()
+
+		mockProxy := &mock.ProxyStub{}
+		nth, err := NewElrondProxyWithCache(mockProxy, time.Millisecond)
+		require.Nil(t, nth)
+		assert.Equal(t, ErrInvalidCacherDuration, err)
+	})
+	t.Run("should work", func(t *testing.T) {
+		t.Parallel()
+
+		mockProxy := &mock.ProxyStub{}
+		nth, err := NewElrondProxyWithCache(mockProxy, minimumCachingInterval*2)
+
+		require.NotNil(t, nth)
+		require.Nil(t, err)
+	})
+}
+
+func TestGetNetworkConfig(t *testing.T) {
+	t.Parallel()
+	expectedReturnedNetworkConfig := &data.NetworkConfig{
+		ChainID:                  "test",
+		Denomination:             1,
+		GasPerDataByte:           2,
+		LatestTagSoftwareVersion: "test",
+		MetaConsensusGroup:       3,
+		MinGasLimit:              4,
+		MinGasPrice:              5,
+		MinTransactionVersion:    6,
+		NumMetachainNodes:        7,
+		NumNodesInShard:          8,
+		NumShardsWithoutMeta:     9,
+		RoundDuration:            10,
+		ShardConsensusGroupSize:  11,
+		StartTime:                12,
+	}
+	t.Run("getCachedConfigs returns nil", func(t *testing.T) {
+		t.Parallel()
+		t.Run("cache time expired", func(t *testing.T) {
+			t.Parallel()
+			mockProxy := &mock.ProxyStub{}
+			wasCalled := false
+			mockProxy.GetNetworkConfigCalled = func() (*data.NetworkConfig, error) {
+				wasCalled = true
+				return expectedReturnedNetworkConfig, nil
+			}
+
+			nth := NewElrondProxyWithCacheWithHandlers(
+				mockProxy,
+				minimumCachingInterval*2,
+				func(t time.Time) time.Duration {
+					return minimumCachingInterval
+				})
+
+			configs, err := nth.GetNetworkConfig(context.Background())
+
+			require.Nil(t, err)
+			require.True(t, wasCalled)
+			assert.Equal(t, expectedReturnedNetworkConfig, configs)
+		})
+		t.Run("fetchedConfigs is nil", func(t *testing.T) {
+			t.Parallel()
+			mockProxy := &mock.ProxyStub{}
+			wasCalled := false
+			mockProxy.GetNetworkConfigCalled = func() (*data.NetworkConfig, error) {
+				wasCalled = true
+				return expectedReturnedNetworkConfig, nil
+			}
+
+			nth := NewElrondProxyWithCacheWithHandlers(
+				mockProxy,
+				minimumCachingInterval*2,
+				func(t time.Time) time.Duration {
+					return minimumCachingInterval*2 + time.Millisecond
+				})
+			nth.fetchedConfigs = nil
+
+			configs, err := nth.GetNetworkConfig(context.Background())
+
+			require.Nil(t, err)
+			require.True(t, wasCalled)
+			assert.Equal(t, expectedReturnedNetworkConfig, configs)
+		})
+		t.Run("and Proxy.GetNetworkConfig returns error", func(t *testing.T) {
+			t.Parallel()
+			expectedError := errors.New("expected error")
+			mockProxy := &mock.ProxyStub{}
+			wasCalled := false
+			mockProxy.GetNetworkConfigCalled = func() (*data.NetworkConfig, error) {
+				wasCalled = true
+				return nil, expectedError
+			}
+
+			nth := NewElrondProxyWithCacheWithHandlers(
+				mockProxy,
+				minimumCachingInterval*2,
+				func(t time.Time) time.Duration {
+					return minimumCachingInterval*2 + time.Millisecond
+				})
+			nth.fetchedConfigs = nil
+
+			configs, err := nth.GetNetworkConfig(context.Background())
+
+			require.Nil(t, configs)
+			require.True(t, wasCalled)
+			assert.Equal(t, expectedError, err)
+		})
+	})
+	t.Run("getCachedConfigs returns valid fetchedConfigs", func(t *testing.T) {
+		t.Parallel()
+		mockProxy := &mock.ProxyStub{}
+		wasCalled := false
+		mockProxy.GetNetworkConfigCalled = func() (*data.NetworkConfig, error) {
+			wasCalled = true
+			return nil, nil
+		}
+
+		nth := NewElrondProxyWithCacheWithHandlers(
+			mockProxy,
+			minimumCachingInterval*2,
+			func(t time.Time) time.Duration {
+				return minimumCachingInterval
+			})
+		nth.fetchedConfigs = expectedReturnedNetworkConfig
+
+		configs, err := nth.GetNetworkConfig(context.Background())
+
+		require.Nil(t, err)
+		assert.False(t, wasCalled)
+		assert.Equal(t, expectedReturnedNetworkConfig, configs)
+	})
+}
