2c519cd9aeaf37a429d7f856d866c40cb405f85c
Rebegea Dragos-Alexandru <dragosalex97@gmail.com>
2021-12-28 10:39:54 +0200

    fix after review

--- blockchain/interface.go
+++ blockchain/interface.go
@@ -8,7 +8,6 @@ import (
 )
 
 // Proxy holds the primitive functions that the elrond proxy engine supports & implements
-// dependency inversion: blockchain package is considered inner business logic, this package is considered "plugin"
 type Proxy interface {
 	GetNetworkConfig(ctx context.Context) (*data.NetworkConfig, error)
 	GetAccount(ctx context.Context, address core.AddressHandler) (*data.Account, error)
