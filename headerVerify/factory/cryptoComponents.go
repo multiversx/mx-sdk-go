557c63d3c6721465c231bda2cc53d4096fc33964
ssd04 <dariussuirab@gmail.com>
2022-01-27 16:05:01 +0200

    headerVerify: refactor crypto components

--- headerVerify/factory/cryptoComponents.go
+++ headerVerify/factory/cryptoComponents.go
@@ -1,29 +1,14 @@
 package factory
 
 import (
-	"fmt"
-
-	"github.com/ElrondNetwork/elrond-go-core/hashing"
 	"github.com/ElrondNetwork/elrond-go-core/hashing/blake2b"
-	"github.com/ElrondNetwork/elrond-go-core/hashing/sha256"
 	crypto "github.com/ElrondNetwork/elrond-go-crypto"
 	"github.com/ElrondNetwork/elrond-go-crypto/signing"
-	disabledCrypto "github.com/ElrondNetwork/elrond-go-crypto/signing/disabled"
 	disabledSig "github.com/ElrondNetwork/elrond-go-crypto/signing/disabled/singlesig"
 	"github.com/ElrondNetwork/elrond-go-crypto/signing/mcl"
 	mclMultiSig "github.com/ElrondNetwork/elrond-go-crypto/signing/mcl/multisig"
-	mclSig "github.com/ElrondNetwork/elrond-go-crypto/signing/mcl/singlesig"
 	"github.com/ElrondNetwork/elrond-go-crypto/signing/multisig"
-	logger "github.com/ElrondNetwork/elrond-go-logger"
-	"github.com/ElrondNetwork/elrond-go/consensus"
-	"github.com/ElrondNetwork/elrond-go/errors"
 )
-
-var log = logger.GetOrCreate("example/leveldb/factory")
-
-var ErrInvalidConsensusConfig = fmt.Errorf("invalid consensus type provided in config file")
-
-const disabledSigChecking = "disabled"
 
 type cryptoComponents struct {
 	KeyGen            crypto.KeyGenerator
@@ -32,22 +17,11 @@ type cryptoComponents struct {
 }
 
 func CreateCryptoComponents() (*cryptoComponents, error) {
-	consensusType := "bls"
-	multisigHasherType := "blake2b"
+	blockSignKeyGen := signing.NewKeyGenerator(mcl.NewSuiteBLS12())
 
-	suite, err := getSuite(consensusType)
-	if err != nil {
-		return nil, err
-	}
+	interceptSingleSigner := &disabledSig.DisabledSingleSig{}
 
-	blockSignKeyGen := signing.NewKeyGenerator(suite)
-
-	interceptSingleSigner, err := createSingleSigner(consensusType)
-	if err != nil {
-		return nil, err
-	}
-
-	multisigHasher, err := getMultiSigHasher(consensusType, multisigHasherType)
+	multisigHasher, err := blake2b.NewBlake2bWithSize(multisig.BlsHashSize)
 	if err != nil {
 		return nil, err
 	}
@@ -59,7 +33,6 @@ func CreateCryptoComponents() (*cryptoComponents, error) {
 		return nil, err
 	}
 
-	//multiSigner, err := createMultiSigner(consensusType, multisigHasher, cp, blockSignKeyGen)
 	multiSigner, err := multisig.NewBLSMultisig(
 		&mclMultiSig.BlsMultiSigner{Hasher: multisigHasher},
 		[]string{string(publicKeyBytes)},
@@ -76,45 +49,4 @@ func CreateCryptoComponents() (*cryptoComponents, error) {
 		SingleSigVerifier: interceptSingleSigner,
 		MultiSigVerifier:  multiSigner,
 	}, nil
-}
-
-func getSuite(consensusType string) (crypto.Suite, error) {
-	switch consensusType {
-	case consensus.BlsConsensusType:
-		return mcl.NewSuiteBLS12(), nil
-	case disabledSigChecking:
-		return disabledCrypto.NewDisabledSuite(), nil
-	default:
-		return nil, ErrInvalidConsensusConfig
-	}
-}
-
-func createSingleSigner(consensusType string) (crypto.SingleSigner, error) {
-	switch consensusType {
-	case consensus.BlsConsensusType:
-		return &mclSig.BlsSingleSigner{}, nil
-	case disabledSigChecking:
-		log.Warn("using disabled single signer")
-		return &disabledSig.DisabledSingleSig{}, nil
-	default:
-		return nil, errors.ErrInvalidConsensusConfig
-	}
-}
-
-func getMultiSigHasher(consensusType string, multisigHasherType string) (hashing.Hasher, error) {
-	if consensusType == consensus.BlsConsensusType && multisigHasherType != "blake2b" {
-		return nil, errors.ErrMultiSigHasherMissmatch
-	}
-
-	switch multisigHasherType {
-	case "sha256":
-		return sha256.NewSha256(), nil
-	case "blake2b":
-		if consensusType == consensus.BlsConsensusType {
-			return blake2b.NewBlake2bWithSize(multisig.BlsHashSize)
-		}
-		return blake2b.NewBlake2b(), nil
-	}
-
-	return nil, errors.ErrMissingMultiHasherConfig
 }
