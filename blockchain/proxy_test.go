0bc6985deadeb552627be0f5ca401eec3d67c3d1
cpda11 <53509553+cpda11@users.noreply.github.com>
2022-07-30 07:04:56 +0200

    Additional response tests, extend client mock for custom status code

--- blockchain/proxy_test.go
+++ blockchain/proxy_test.go
@@ -46,11 +46,15 @@ func (m *mockHTTPClient) Do(req *http.Request) (*http.Response, error) {
 }
 
 func createMockClientRespondingBytes(responseBytes []byte) *mockHTTPClient {
+	return createMockClientRespondingBytesWithStatusCode(responseBytes, http.StatusOK)
+}
+
+func createMockClientRespondingBytesWithStatusCode(responseBytes []byte, status int) *mockHTTPClient {
 	return &mockHTTPClient{
 		doCalled: func(req *http.Request) (*http.Response, error) {
 			return &http.Response{
 				Body:       ioutil.NopCloser(bytes.NewReader(responseBytes)),
-				StatusCode: http.StatusOK,
+				StatusCode: status,
 			}, nil
 		},
 	}
@@ -617,13 +621,13 @@ func TestElrondProxy_GetAccountKeys(t *testing.T) {
 		expectedAccountKeys = &data.AccountKeys{
 			"666f6f": "626172", // "key": "value"
 		}
-		defaultResponseBytes = []byte("{\"data\":{\"pairs\":{\"666f6f\":\"626172\"}},\"error\":\"\",\"code\":\"\"}") // "key": "value"
+		defaultResponseBytes = []byte(`{"data":{"pairs":{"666f6f":"626172"}},"error":"","code":""}`) // "key": "value"
 	)
 
 	address, err := data.NewAddressFromBech32String("erd1qqqqqqqqqqqqqpgqfzydqmdw7m2vazsp6u5p95yxz76t2p9rd8ss0zp9ts")
-	if err != nil {
-		assert.Error(t, err)
-	}
+	assert.NotNil(t, address)
+	assert.Nil(t, err)
+
 	t.Run("retrieve all account storage data", func(t *testing.T) {
 		httpClient := createMockClientRespondingBytes(defaultResponseBytes)
 		args := createMockArgsElrondProxy(httpClient)
@@ -669,6 +673,26 @@ func TestElrondProxy_GetAccountKeys(t *testing.T) {
 		response, err := proxy.GetAccountKeys(context.Background(), address)
 		assert.NotNil(t, err)
 		assert.IsType(t, &json.SyntaxError{}, err)
+		assert.Equal(t, (*data.AccountKeys)(nil), response)
+	})
+	t.Run("fail on response error status", func(t *testing.T) {
+		httpClient := createMockClientRespondingBytesWithStatusCode(defaultResponseBytes, http.StatusBadRequest)
+		args := createMockArgsElrondProxy(httpClient)
+		proxy, _ := NewElrondProxy(args)
+
+		response, err := proxy.GetAccountKeys(context.Background(), address)
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "Bad Request")
+		assert.Nil(t, response)
+	})
+	t.Run("fail on response error message", func(t *testing.T) {
+		httpClient := createMockClientRespondingBytes([]byte(`{"data":{"pairs":{"666f6f":"626172"}},"error":"fail","code":""}`))
+		args := createMockArgsElrondProxy(httpClient)
+		proxy, _ := NewElrondProxy(args)
+
+		response, err := proxy.GetAccountKeys(context.Background(), address)
+		assert.NotNil(t, err)
+		assert.IsType(t, errors.New(""), err)
 		assert.Equal(t, (*data.AccountKeys)(nil), response)
 	})
 }
