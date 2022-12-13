44230595717b7088d76372ebf1266e3abd373a62
schimih <schiopumihaialexandru@gmail.com>
2022-12-13 10:15:54 +0200

    fix naming

--- disabled/cache.go
+++ disabled/cache.go
@@ -5,15 +5,15 @@ type Cache struct {
 }
 
 // Clear does nothing
-func (rm *Cache) Clear() {
+func (c *Cache) Clear() {
 }
 
 // Put returns false
-func (rm *Cache) Put(key []byte, value interface{}, sizeInBytes int) (evicted bool) {
+func (c *Cache) Put(key []byte, value interface{}, sizeInBytes int) (evicted bool) {
 	return false
 }
 
 // Get returns false
-func (rm *Cache) Get(key []byte) (value interface{}, ok bool) {
+func (c *Cache) Get(key []byte) (value interface{}, ok bool) {
 	return nil, false
 }
