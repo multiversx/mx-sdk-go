adc4068e9dedf1b143baa57cfb705e1101722f39
radu chis <radu.chis@elrond.com>
2023-01-26 14:12:43 +0200

    proxy test renamed

--- blockchain/proxy_test.go
+++ blockchain/proxy_test.go
@@ -253,7 +253,7 @@ func TestGetAccount(t *testing.T) {
 	})
 }
 
-func TestMultiversXProxy_GetNetworkEconomics(t *testing.T) {
+func TestProxy_GetNetworkEconomics(t *testing.T) {
 	t.Parallel()
 
 	responseBytes := []byte(`{"data":{"metrics":{"erd_dev_rewards":"0","erd_epoch_for_economics_data":263,"erd_inflation":"5869888769785838708144","erd_total_fees":"51189055176110000000","erd_total_staked_value":"9963775651405816710680128","erd_total_supply":"21556417261819025351089574","erd_total_top_up_value":"1146275808171377418645274"}},"code":"successful"}`)
@@ -274,7 +274,7 @@ func TestMultiversXProxy_GetNetworkEconomics(t *testing.T) {
 	}, networkEconomics)
 }
 
-func TestMultiversXProxy_RequestTransactionCost(t *testing.T) {
+func TestProxy_RequestTransactionCost(t *testing.T) {
 	t.Parallel()
 
 	responseBytes := []byte(`{"data":{"txGasUnits":24273810,"returnMessage":""},"error":"","code":"successful"}`)
@@ -300,7 +300,7 @@ func TestMultiversXProxy_RequestTransactionCost(t *testing.T) {
 	}, txCost)
 }
 
-func TestMultiversXProxy_GetTransactionInfoWithResults(t *testing.T) {
+func TestProxy_GetTransactionInfoWithResults(t *testing.T) {
 	t.Parallel()
 
 	responseBytes := []byte(`{"data":{"transaction":{"type":"normal","nonce":22100,"round":53057,"epoch":263,"value":"0","receiver":"erd1e6c9vcga5lyhwdu9nr9lya4ujz2r5w2egsfjp0lslrgv5dsccpdsmre6va","sender":"erd1e6c9vcga5lyhwdu9nr9lya4ujz2r5w2egsfjp0lslrgv5dsccpdsmre6va","gasPrice":1000000001,"gasLimit":19210000,"data":"RVNEVE5GVFRyYW5zZmVyQDU5NTk1OTQ1MzY0MzM5MkQzMDM5MzQzOTMwMzBAMDFAMDFAMDAwMDAwMDAwMDAwMDAwMDA1MDA1QzgzRTBDNDJFRENFMzk0RjQwQjI0RDI5RDI5OEIwMjQ5QzQxRjAyODk3NEA2Njc1NkU2NEA1N0M1MDZBMTlEOEVBRTE4Mjk0MDNBOEYzRjU0RTFGMDM3OUYzODE1N0ZDRjUzRDVCQ0E2RjIzN0U0QTRDRjYxQDFjMjA=","signature":"37922ccb13d46857d819cd618f1fd8e76777e14f1c16a54eeedd55c67d5883db0e3a91cd0ff0f541e2c3d7347488b4020c0ba4765c9bc02ea0ae1c3f6db1ec05","sourceShard":1,"destinationShard":1,"blockNonce":53052,"blockHash":"4a63312d1bfe48aa516185d12abff5daf6343fce1f298db51291168cf97a790c","notarizedAtSourceInMetaNonce":53053,"NotarizedAtSourceInMetaHash":"342d189e36ef5cbf9f8b3f2cb5bf2cb8e2260062c6acdf89ce5faacd99f4dbcc","notarizedAtDestinationInMetaNonce":53053,"notarizedAtDestinationInMetaHash":"342d189e36ef5cbf9f8b3f2cb5bf2cb8e2260062c6acdf89ce5faacd99f4dbcc","miniblockType":"TxBlock","miniblockHash":"0c659cce5e2653522cc0e3cf35571264522035a7aef4ffa5244d1ed3d8bc01a8","status":"success","hyperblockNonce":53053,"hyperblockHash":"342d189e36ef5cbf9f8b3f2cb5bf2cb8e2260062c6acdf89ce5faacd99f4dbcc","smartContractResults":[{"hash":"5ab14959aaeb3a20d95ec6bbefc03f251732d9368711c55c63d3811e70903f4e","nonce":0,"value":0,"receiver":"erd1qqqqqqqqqqqqqpgqtjp7p3pwmn3efaqtynff62vtqfyug8cz396qs5vnsy","sender":"erd1e6c9vcga5lyhwdu9nr9lya4ujz2r5w2egsfjp0lslrgv5dsccpdsmre6va","data":"ESDTNFTTransfer@595959453643392d303934393030@01@01@080112020001226f08011204746573741a20ceb056611da7c977378598cbf276bc90943a3959441320bff0f8d0ca3618c05b20e8072a206161616161616161616161616161616161616161616161616161616161616161320461626261321268747470733a2f2f656c726f6e642e636f6d3a0474657374@66756e64@57c506a19d8eae1829403a8f3f54e1f0379f38157fcf53d5bca6f237e4a4cf61@1c20","prevTxHash":"c7fadeaccce0673bd6ce3a1f472f7cc1beef20c0b3131cfa9866cd5075816639","originalTxHash":"c7fadeaccce0673bd6ce3a1f472f7cc1beef20c0b3131cfa9866cd5075816639","gasLimit":18250000,"gasPrice":1000000001,"callType":0},{"hash":"f337d2705d2b644f9d8f75cf270e879b6ada51c4c54009e94305adf368d1adbc","nonce":1,"value":102793170000000,"receiver":"erd1e6c9vcga5lyhwdu9nr9lya4ujz2r5w2egsfjp0lslrgv5dsccpdsmre6va","sender":"erd1qqqqqqqqqqqqqpgqtjp7p3pwmn3efaqtynff62vtqfyug8cz396qs5vnsy","data":"@6f6b","prevTxHash":"5ab14959aaeb3a20d95ec6bbefc03f251732d9368711c55c63d3811e70903f4e","originalTxHash":"c7fadeaccce0673bd6ce3a1f472f7cc1beef20c0b3131cfa9866cd5075816639","gasLimit":0,"gasPrice":1000000001,"callType":0}]}},"error":"","code":"successful"}`)
@@ -315,7 +315,7 @@ func TestMultiversXProxy_GetTransactionInfoWithResults(t *testing.T) {
 	fmt.Println(string(txBytes))
 }
 
-func TestMultiversXProxy_ExecuteVmQuery(t *testing.T) {
+func TestProxy_ExecuteVmQuery(t *testing.T) {
 	t.Parallel()
 
 	responseBytes := []byte(`{"data":{"data":{"returnData":["MC41LjU="],"returnCode":"ok","returnMessage":"","gasRemaining":18446744073685949187,"gasRefund":0,"outputAccounts":{"0000000000000000050033bb65a91ee17ab84c6f8a01846ef8644e15fb76696a":{"address":"erd1qqqqqqqqqqqqqpgqxwakt2g7u9atsnr03gqcgmhcv38pt7mkd94q6shuwt","nonce":0,"balance":null,"balanceDelta":0,"storageUpdates":{},"code":null,"codeMetaData":null,"outputTransfers":[],"callType":0}},"deletedAccounts":[],"touchedAccounts":[],"logs":[]}},"error":"","code":"successful"}`)
@@ -416,7 +416,7 @@ func TestMultiversXProxy_ExecuteVmQuery(t *testing.T) {
 	})
 }
 
-func TestMultiversXProxy_GetRawBlockByHash(t *testing.T) {
+func TestProxy_GetRawBlockByHash(t *testing.T) {
 	t.Parallel()
 
 	expectedTs := &testStruct{
@@ -448,7 +448,7 @@ func TestMultiversXProxy_GetRawBlockByHash(t *testing.T) {
 	require.Equal(t, expectedTs, ts)
 }
 
-func TestMultiversXProxy_GetRawBlockByNonce(t *testing.T) {
+func TestProxy_GetRawBlockByNonce(t *testing.T) {
 	t.Parallel()
 
 	expectedTs := &testStruct{
@@ -478,7 +478,7 @@ func TestMultiversXProxy_GetRawBlockByNonce(t *testing.T) {
 	require.Equal(t, expectedTs, ts)
 }
 
-func TestMultiversXProxy_GetRawMiniBlockByHash(t *testing.T) {
+func TestProxy_GetRawMiniBlockByHash(t *testing.T) {
 	t.Parallel()
 
 	expectedTs := &testStruct{
@@ -508,7 +508,7 @@ func TestMultiversXProxy_GetRawMiniBlockByHash(t *testing.T) {
 	require.Equal(t, expectedTs, ts)
 }
 
-func TestMultiversXProxy_GetNonceAtEpochStart(t *testing.T) {
+func TestProxy_GetNonceAtEpochStart(t *testing.T) {
 	t.Parallel()
 
 	expectedNonce := uint64(2)
@@ -535,7 +535,7 @@ func TestMultiversXProxy_GetNonceAtEpochStart(t *testing.T) {
 	require.Equal(t, expectedNonce, response)
 }
 
-func TestMultiversXProxy_GetRatingsConfig(t *testing.T) {
+func TestProxy_GetRatingsConfig(t *testing.T) {
 	t.Parallel()
 
 	expectedRatingsConfig := &data.RatingsConfig{
@@ -561,7 +561,7 @@ func TestMultiversXProxy_GetRatingsConfig(t *testing.T) {
 	require.Equal(t, expectedRatingsConfig, response)
 }
 
-func TestMultiversXProxy_GetEnableEpochsConfig(t *testing.T) {
+func TestProxy_GetEnableEpochsConfig(t *testing.T) {
 	t.Parallel()
 
 	enableEpochs := config.EnableEpochs{
@@ -590,7 +590,7 @@ func TestMultiversXProxy_GetEnableEpochsConfig(t *testing.T) {
 	require.Equal(t, expectedEnableEpochsConfig, response)
 }
 
-func TestMultiversXProxy_GetGenesisNodesPubKeys(t *testing.T) {
+func TestProxy_GetGenesisNodesPubKeys(t *testing.T) {
 	t.Parallel()
 
 	expectedGenesisNodes := &data.GenesisNodes{
@@ -616,7 +616,7 @@ func TestMultiversXProxy_GetGenesisNodesPubKeys(t *testing.T) {
 	require.Equal(t, expectedGenesisNodes, response)
 }
 
-func TestMultiversXProxy_GetValidatorsInfoByEpoch(t *testing.T) {
+func TestProxy_GetValidatorsInfoByEpoch(t *testing.T) {
 	t.Parallel()
 
 	expectedValidatorsInfo := []*state.ShardValidatorInfo{
