33ccc3e5f26707472e8715d2377447230451796f
Alexander Cristurean <alexander.cristurean@yahoo.com>
2023-12-11 14:47:26 +0200

    initial wallet module.

--- mx-sdk-go-old/blockchain/cryptoProvider/errors.go
+++
@@ -1,6 +0,0 @@
-package cryptoProvider
-
-import "errors"
-
-// ErrTxAlreadySigned signals that the provided transaction is already signed
-var ErrTxAlreadySigned = errors.New("tx already signed")
--- mx-sdk-go-old/examples/networks.go
+++
@@ -1,9 +0,0 @@
-package examples
-
-// MainnetGateway represents the mainnet gateway address
-const MainnetGateway = "https://gateway.multiversx.com"
-
-// TestnetGateway represents the testnet gateway address
-const TestnetGateway = "https://testnet-gateway.multiversx.com"
-
-const TestnetAPI = "https://testnet-api.multiversx.com"
